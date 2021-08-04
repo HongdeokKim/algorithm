e20155a6f7e3fd0ea26ef41617da321361482a3f
HongdeokKim <76855335+HongdeokKim@users.noreply.github.com>
2021-08-04 23:55:53 +0900

    210804

--- baekjoon/단계/08 기본 수학 1 /2775 부녀회장이 될테야.py
+++ baekjoon/단계/08 기본 수학 1 /2775 부녀회장이 될테야.py
@@ -1,12 +1,16 @@
 T = int(input())
+s = []
 
 for _ in range(T):
     k = int(input())
     n = int(input())
 
-    l = [[i for i in range(1, n+1)]]
+    l = [i for i in range(1, n+1)]
 
-    for i in range(1, k):
-        l.append([sum(l[j][:j]) for j in range(1, n)])
-    print(l)
+    for i in range(k):
+        for j in range(1, n):
+            l[j] = l[j - 1] + l[j]
+    s.append(l[n - 1])
+
+print("\n".join(map(str, s)))
     
