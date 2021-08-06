90e3c507fff97c3fff9c80307d80f19634f64f48
HongdeokKim <76855335+HongdeokKim@users.noreply.github.com>
2021-08-06 23:37:14 +0900

    210806

--- baekjoon/단계/08 기본 수학 1 /10250 ACM 호텔.py
+++ baekjoon/단계/08 기본 수학 1 /10250 ACM 호텔.py
@@ -3,8 +3,8 @@ T = int(input())
 for _ in range(T):
     H, W, N = map(int, input().split())
     
-    if N & H == 0:
-        print((N % H) * 100 + H)
+    if N % H == 0:
+        print(H * 100 + N // H)
     else:
-        print((N % H) * 100 + (N // H) + 1)
+        print((N % H) * 100 + N // H + 1)
 
