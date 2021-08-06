a42d8994e5c3ac689c1c7e33a83862ff1f6dfbd7
HongdeokKim <76855335+HongdeokKim@users.noreply.github.com>
2021-08-06 23:48:41 +0900

    210806

--- baekjoon/단계/08 기본 수학 1 /2869 달팽이는 올라가고 싶다.py
+++ baekjoon/단계/08 기본 수학 1 /2869 달팽이는 올라가고 싶다.py
@@ -1,9 +1,7 @@
+import math
+
 A, B, V = map(int, input().split())
 
-h = A # 낮
-w = A - B
-day = 1
-while h < V:
-    h += w # 밤
-    day += 1
-print(day)
+print(math.ceil((V - A) / (A - B) + 1))
+
+# math 라이브러리를 안쓰고 +.99 -> int()를 통해 같은 역할을 수행할 수 있음
