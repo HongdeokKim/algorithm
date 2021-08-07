e54f5b09c57109750ae60b08c1504cb6cae178cb
HongdeokKim <76855335+HongdeokKim@users.noreply.github.com>
2021-08-07 23:30:42 +0900

    210807

--- baekjoon/단계/08 기본 수학 1 /1193 분수찾기.py
+++ baekjoon/단계/08 기본 수학 1 /1193 분수찾기.py
@@ -1,24 +1,32 @@
-N = int(input())
-n = 1
-cnt = 1
-while cnt < N:
-    n += 1
-    cnt += n
-print(n, cnt)
 
-n -= 1
-cnt -= 1
 
-if n % 2 == 0:
-    pass
-else:
-    pass
+def find_index(N):
+    n = 1
+    while n * (n + 1) / 2 < N:
+        n += 1
 
+    temp = N - ((n-1) * n) / 2
+    print(temp)
+    print(n)
+    if n % 2 == 0: # 짝수
+        print("%d/%d" % (1 + temp, n + 1 - temp))
+    else: # 홀수
+        pass
+    
+    
+
+    return n
+
+while True:
+    N = int(input())
+    find_index(N)
 '''
 0 0
 
 1 0
 0 1
+
+n(n + 1) < 28
 
 0 2
 1 1
