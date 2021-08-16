7a0a4dc9d10988006cb7efe8fa44148274778a19
HongdeokKim <76855335+HongdeokKim@users.noreply.github.com>
2021-08-16 23:28:07 +0900

    210816

--- 이코테/06 정렬/6-3 퀵 정렬.py
+++ 이코테/06 정렬/6-3 퀵 정렬.py
@@ -2,9 +2,26 @@ arr = [1, 3, 2, 4, 9, 5, 8, 6, 7, 0]
 
 def quick_sort(arr, start, end):
     if start >= end:
-        pivot = start
-        left  = start + 1
-        right = end
+        return
+    
+    pivot = start
+    left  = start + 1
+    right = end
 
-        while left <= right:
-            while
+    while left <= right:
+        while left < end and arr[left] <= arr[pivot]:
+            left += 1
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    quick_sort(arr, start, right - 1)
+    quick_sort(arr, right + 1, end)
+    
+quick_sort(arr, 0, len(arr) - 1)
+
+print(arr)
+
+        
