5cbea33c712e2614a824cc4ef236369aa11d7926
HongdeokKim <qchdkim@naver.com>
2021-09-15 04:04:45 +0900

    210915

--- 이코테/05 DFS, BFS/5-7 음료수 얼려 먹기.py
+++ 이코테/05 DFS, BFS/5-7 음료수 얼려 먹기.py
@@ -1,3 +1,44 @@
+# l = []
+# cnt = 0
+
+# N, M = map(int, input().split())
+# for _ in range(N):
+#     l.append(input())
+
+# table = [[False] * M] * N
+
+
+# for i in range(N):
+#     for j in range(M):
+#         table[N][M] = True
+
+#         if table[N][j] == False and l[N][M] == '0':
+            
+
 N, M = map(int, input().split())
 
-print(N, M)
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] == 1
+
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+result = 0
+for i in range(N):
+    for j in range(M):
+        if dfs(i, j) == True:
+            result += 1
+print(result)
+
