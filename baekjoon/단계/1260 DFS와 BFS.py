d161f46d7ef3a8e7557cbce2a9662e4ce23636f1
HongdeokKim <qchdkim@naver.com>
2022-01-24 05:01:07 +0900

    220124

---
+++ baekjoon/단계/1260 DFS와 BFS.py
@@ -0,0 +1,33 @@
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+def DFS(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(graph, i, visited)
+
+def BFS(graph, v, visited):
+    visited = [False] * (N + 1)
+    queue = deque([v])
+
+N, M, V = map(int, input().split())
+graph = []
+for _ in range(N + 1):
+    graph.append([])
+visited = [False] * (N + 1)
+
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+DFS(graph, 1, visited)
+
+
+
+
+# def BFS():
