9ec44b8d481bb10eb96d65ba209af83ac2a80887
HongdeokKim <qchdkim@naver.com>
2022-07-15 23:41:42 +0900

    220715

--- baekjoon/단계/21 분할정복/2630 색종이 만들기.py
+++ baekjoon/단계/21 분할정복/2630 색종이 만들기.py
@@ -1,9 +1,33 @@
 from sys import stdin
 input = stdin.readline
 
+N = (int(input()))
 P = []
+blue  = 0
+white = 0
 
-print(1 / 2)
-for _ in range (int(input())):
+for _ in range(N):
     P.append(list(map(int, input().split())))
 
+def find(x, y, size):
+    global blue, white
+    color = P[x][y]
+    
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if P[i][j] != color:
+                find(x, y, size // 2)
+                find(x, y + size // 2, size // 2)
+                find(x + size // 2, y, size // 2)
+                find(x + size // 2, y + size // 2, size // 2)
+                return
+    if color == 0:
+        white = white + 1
+        return
+    else:
+        blue = blue + 1
+        return
+
+find(0, 0, N)
+print(white)
+print(blue)
