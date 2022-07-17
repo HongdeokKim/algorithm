c49cae023dd8fe80efd3e11c29d24d64691ed5ed
HongdeokKim <qchdkim@naver.com>
2022-07-17 22:02:05 +0900

    220717

--- baekjoon/단계/21 분할정복/1992 쿼드트리/Solution.java
+++ baekjoon/단계/21 분할정복/1992 쿼드트리/Solution.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class Solution {
-    pubilc static void main(String[] args) {
+    
+    public static int[][] img;
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			
+			for(int j = 0; j < N; j++) {
+				img[i][j] = str.charAt(j) - '0';
+			}
+		}
     }
 }
