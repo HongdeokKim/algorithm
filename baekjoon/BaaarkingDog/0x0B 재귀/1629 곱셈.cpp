75b4026653de2ae6d0a328f4b369678b30d62493
HongdeokKim <qchdkim@naver.com>
2022-11-26 15:11:03 +0900

    221126

--- baekjoon/BaaarkingDog/0x0B 재귀/1629 곱셈.cpp
+++ baekjoon/BaaarkingDog/0x0B 재귀/1629 곱셈.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-using long ll;
+using ll = long long;
+
+ll pow(ll a, ll b, ll c) {
+    int temp; // 자료형 생각해야했음!
+    if(b == 1) return a % c; // terminal
+    else if(b % 2) return (pow(a, b - 1, c) * a) % c; // 홀수라면
+    else { // 짝수라면
+        temp = pow(a, b / 2, c);
+        return (temp * temp) % c;
+    }
+}
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
     int A, B, C;
-    long long res = 1;
-
     cin >> A >> B >> C;
-    
-    for(int i = 0; i < B; i++) {
-        res = res * A;
-    }
-
-    cout << res % C << endl;
+    cout << pow(A, B, C) << endl;
 
     return 0;
 }
 
 // 재귀, 분할정복
+// (A * A * A) % C == (A % C)(A % C)(A % C)
+// temp 자료형에서 오버플로우가 날 수도 있음
+// https://chan9.tistory.com/78
